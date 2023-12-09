dea2d1582bfeefbf545d4933952da62ab369703b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-12-09 10:49:47 +0000

    Solve 2023 day 09

---
+++ 2023/09/1.py
@@ -0,0 +1,12 @@
+with open("input") as f:
+    lines = [[int(n) for n in line.split()] for line in f.read().strip().split("\n")]
+
+out = 0
+for line in lines:
+    big_lines = [line]
+    while len(set(big_lines[-1])) > 1:
+        big_lines.append([big_lines[-1][i+1]-big_lines[-1][i] for i in range(len(big_lines[-1])-1)])
+    for i in reversed(range(len(big_lines)-1)):
+        big_lines[i].append(big_lines[i][-1]+big_lines[i+1][-1])
+    out += big_lines[0][-1]
+print(out)
