a19374bab2229aeee1e62a9cbacb94cdce32ce76
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-12-23 23:06:01 +0000

    wip

--- 2023/23/2.py
+++ 2023/23/2.py
@@ -1,6 +1,9 @@
-with open("input") as f:
-    lines = [["#" if c == "#" else " " for c in line]
-             for line in f.read().strip().split("\n")]
+with open("example") as f:
+    lines = list(map(list, f.read().strip().split("\n")))
+
+class Node:
+    def __init__(self):
+        self.links = []
 
 directions = ((0, 1), (0, -1), (-1, 0), (1, 0))
 exit_x, exit_y = len(lines) - 1, len(lines[0]) - 2
@@ -12,38 +15,12 @@ def valid_directions(x, y):
         ty = y + dy
         if (0 <= tx < len(lines) and
             0 <= ty < len(lines[0]) and
-            lines[tx][ty] == " "):
+            lines[tx][ty] != "#"):
             out.append((tx, ty))
     return out
 
-def clean_path(path):
-    for x, y in path:
-        lines[x][y] = " "
-
-def print_lines_coloured():
-    for line in lines:
-        for c in line:
-            if c == "O":
-                print(f"\033[91m{c}\033[0m", end="")
-                "]]" # lsp doesnt understand brackets
-            else:
-                print(c, end="")
-        print()
-    print()
-
-
-def debug(f):
-    def wrapper(*args, **kwargs):
-        print(f"{f.__name__}({args}) -> ?")
-        out = f(*args, **kwargs)
-        print(f"{f.__name__}({args}) -> {len(out)}")
-        return out
-    return wrapper
-
-#@debug
-def dfs(x, y):
-    path = [(x, y)]
-    lines[x][y] = "O"
+def dfs(x, y, parent):
+    lines[x][y] = "#"
     cur_directions = valid_directions(x, y)
     while len(cur_directions) == 1:
         x, y = cur_directions[0]
@@ -71,6 +48,8 @@ def dfs(x, y):
     return max(results, key=len) + path
 
 
+nodes = {}
 x, y = 0, 1
+nodes[(x, y)] = Node()
 path = dfs(x, y)
 print(len(path)-1)
