cb6fdcf7b2165a09c423c761d08cc3b818b4e5b0
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-12-04 16:15:32 +0000

    solve part 2 for example

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,7 +1,7 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn parse() -> (Vec<u32>, Vec<u32>) {
-    let content = fs::read_to_string("src/day01/input.txt").unwrap();
+    let content = fs::read_to_string("src/day01/example.txt").unwrap();
     let lines = content.trim().lines();
     let mut left = Vec::<u32>::new();
     let mut right = Vec::<u32>::new();
@@ -23,4 +23,19 @@ pub fn part1() {
         .map(|(x, y)| x.abs_diff(y))
         .sum();
     println!("{dist}")
+}
+
+pub fn part2() {
+    let (left, right) = parse();
+    let counts = right
+        .into_iter()
+        .fold(HashMap::<u32, u32>::new(), |mut acc, item| {
+            *acc.entry(item).or_insert(0) += 1;
+            acc
+        });
+    let sum: u32 = left
+        .iter()
+        .filter_map(|num| counts.get(num).map(|&val2| num * val2))
+        .sum();
+    println!("{sum}");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod day01;
 
 fn main() {
     day01::part1();
+    day01::part2();
 }
