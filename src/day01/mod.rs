a740d6523e02c85fa2f8e9e3ede0a15911df5743
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-12-04 15:50:33 +0000

    part 1 example

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
 pub fn part1() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("src/day01/example.txt").unwrap();
+    let lines = content.trim().lines();
+    let mut left = Vec::<u32>::new();
+    let mut right = Vec::<u32>::new();
+    for line in lines {
+        let mut nums = line.split_whitespace();
+        left.push(nums.next().unwrap().parse().unwrap());
+        right.push(nums.next().unwrap().parse().unwrap());
+    }
+    left.sort();
+    right.sort();
+    let dist: u32 = left
+        .into_iter()
+        .zip(right)
+        .map(|(x, y)| x.abs_diff(y))
+        .sum();
+    println!("{dist}")
 }
