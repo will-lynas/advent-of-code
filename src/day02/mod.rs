7a052971dc28906b5ea23d6af3abc9d3b91e404d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-12-04 19:13:01 +0000

    part 1 parser

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -1,3 +1,17 @@
-pub fn part1() {}
+use std::fs;
+
+pub fn part1() {
+    let content = fs::read_to_string("src/day02/example.txt").unwrap();
+    let nums: Vec<Vec<u32>> = content
+        .trim()
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect()
+        })
+        .collect();
+    print!("{nums:?}")
+}
 
 pub fn part2() {}
