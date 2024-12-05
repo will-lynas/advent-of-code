f9d5fb485093ae8b2f12e69f1f8c27334308d1ea
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-12-05 12:00:32 +0000

    day 2 part 1

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -1,17 +1,22 @@
 use std::fs;
 
+use itertools::Itertools;
+
 pub fn part1() {
-    let content = fs::read_to_string("src/day02/example.txt").unwrap();
-    let nums: Vec<Vec<u32>> = content
+    let content = fs::read_to_string("src/day02/input.txt").unwrap();
+    let count: usize = content
         .trim()
         .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .map(|s| s.parse().unwrap())
-                .collect()
+        .filter(|line| {
+            let mut nums = line
+                .split_whitespace()
+                .map(|s| s.parse::<i32>().unwrap())
+                .tuple_windows()
+                .map(|(a, b)| a - b);
+            nums.clone().all(|n| (1..=3).contains(&n)) || nums.all(|n| (-3..=-1).contains(&n))
         })
-        .collect();
-    print!("{nums:?}")
+        .count();
+    print!("{count:?}")
 }
 
 pub fn part2() {}
