22f873b20e24590e169614b7ecbc0b668f704290
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-12-05 15:27:25 +0000

    factor out parsing

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -2,24 +2,26 @@ use std::fs;
 
 use itertools::Itertools;
 
-fn is_safe(nums: Vec<i32>) -> bool {
+fn lines() -> Vec<Vec<i32>> {
+    let content = fs::read_to_string("src/day02/input.txt").unwrap();
+    content
+        .trim()
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn is_safe(nums: &[i32]) -> bool {
     let mut nums = nums.iter().tuple_windows().map(|(a, b)| a - b);
     nums.clone().all(|n| (1..=3).contains(&n)) || nums.all(|n| (-3..=-1).contains(&n))
 }
 
 pub fn part1() {
-    let content = fs::read_to_string("src/day02/input.txt").unwrap();
-    let count: usize = content
-        .trim()
-        .lines()
-        .filter(|line| {
-            let nums: Vec<i32> = line
-                .split_whitespace()
-                .map(|s| s.parse().unwrap())
-                .collect();
-            is_safe(nums)
-        })
-        .count();
+    let count = lines().iter().filter(|line| is_safe(line)).count();
     println!("{count:?}")
 }
 
