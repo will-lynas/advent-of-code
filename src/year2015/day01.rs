abab40dab91d3408cd07af065443ead29cc5a99d
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-12-27 15:37:54 +0000

    solve 2015 day2

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -11,8 +11,21 @@ pub fn part1(input: &str) -> i32 {
     floor
 }
 
-pub fn part2(_input: &str) -> u32 {
-    0
+pub fn part2(input: &str) -> usize {
+    let mut floor = 0;
+    for (i, c) in input.trim().chars().enumerate() {
+        if c == '(' {
+            floor += 1
+        } else {
+            floor -= 1
+        }
+
+        if floor < 0 {
+            return i + 1;
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
