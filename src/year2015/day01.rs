2e9e4c950c65e09f9c151ca1f0cd1e5b48bbff85
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-12-27 15:35:41 +0000

    solve 2015 day1 part1

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -1,5 +1,14 @@
-pub fn part1(_input: &str) -> u32 {
-    0
+pub fn part1(input: &str) -> i32 {
+    let mut floor = 0;
+    for c in input.trim().chars() {
+        if c == '(' {
+            floor += 1
+        } else {
+            floor -= 1
+        }
+    }
+
+    floor
 }
 
 pub fn part2(_input: &str) -> u32 {
