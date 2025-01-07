52a17bb8cfbc2669a30fbef1bee6529f69b8c21e
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-07 21:41:59 +0000

    Store Grid as a 1D Vec

--- src/utils/grid.rs
+++ src/utils/grid.rs
@@ -4,40 +4,39 @@ use super::point::Point;
 
 #[derive(Clone)]
 pub struct Grid<T> {
-    pub body: Vec<Vec<T>>,
+    pub width: i32,
+    pub height: i32,
+    pub body: Vec<T>,
 }
 
 impl Grid<u8> {
     pub fn parse(input: &str) -> Self {
+        let bytes: Vec<_> = input.trim().lines().map(str::as_bytes).collect();
+        let height = bytes.len() as i32;
+        let width = bytes[0].len() as i32;
+        let mut body = Vec::with_capacity((width * height) as usize);
+        bytes.iter().for_each(|slice| body.extend_from_slice(slice));
         Grid {
-            body: input
-                .trim()
-                .lines()
-                .map(|line| line.bytes().collect())
-                .collect(),
+            width,
+            height,
+            body,
         }
     }
 }
 
 impl<T> Grid<T> {
     pub fn contains(&self, point: Point) -> bool {
-        point.y >= 0
-            && point.y < self.body.len() as i32
-            && point.x >= 0
-            && point.x < self.body[0].len() as i32
+        point.y >= 0 && point.y < self.height && point.x >= 0 && point.x < self.width
     }
 }
 
-impl<T: PartialEq> Grid<T> {
+impl<T: PartialEq + Copy> Grid<T> {
     pub fn find(&self, goal: &T) -> Option<Point> {
-        for (i, line) in self.body.iter().enumerate() {
-            for (j, c) in line.iter().enumerate() {
-                if c == goal {
-                    return Some(Point::new(j as i32, i as i32));
-                }
-            }
-        }
-        None
+        self.body.iter().position(|b| b == goal).map(|index| {
+            let x = (index as i32) % self.width;
+            let y = (index as i32) / self.width;
+            Point::new(x, y)
+        })
     }
 }
 
@@ -45,12 +44,12 @@ impl<T> Index<Point> for Grid<T> {
     type Output = T;
 
     fn index(&self, point: Point) -> &Self::Output {
-        &self.body[point.y as usize][point.x as usize]
+        &self.body[(self.width * point.y + point.x) as usize]
     }
 }
 
 impl<T> IndexMut<Point> for Grid<T> {
     fn index_mut(&mut self, point: Point) -> &mut Self::Output {
-        &mut self.body[point.y as usize][point.x as usize]
+        &mut self.body[(self.width * point.y + point.x) as usize]
     }
 }
