e9bbe5cbd4b8d72c60ab3ecf2452a992fc78de6f
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-07 17:36:49 +0000

    Add missing semicolons

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -6,9 +6,9 @@ pub fn part1(input: &str) -> i32 {
     let mut floor = 0;
     for c in input.trim().chars() {
         if c == '(' {
-            floor += 1
+            floor += 1;
         } else {
-            floor -= 1
+            floor -= 1;
         }
     }
 
@@ -19,9 +19,9 @@ pub fn part2(input: &str) -> usize {
     let mut floor = 0;
     for (i, c) in input.trim().chars().enumerate() {
         if c == '(' {
-            floor += 1
+            floor += 1;
         } else {
-            floor -= 1
+            floor -= 1;
         }
 
         if floor < 0 {
