d5e86dafe114ff02b696c3a47a42cd8a1126e935
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-07 16:56:42 +0000

    Convert day5 tests

---
+++ tests/year2024/day05.rs
@@ -0,0 +1,43 @@
+use advent_of_code::year2024::day05 as solution;
+use indoc::indoc;
+
+const EXAMPLE: &str = indoc! {"
+    47|53
+    97|13
+    97|61
+    97|47
+    75|29
+    61|13
+    75|53
+    29|13
+    97|29
+    53|29
+    61|53
+    97|53
+    61|29
+    47|13
+    75|47
+    97|75
+    47|61
+    75|61
+    47|29
+    75|13
+    53|13
+
+    75,47,61,53,29
+    97,61,53,29,13
+    75,29,13
+    75,97,47,61,53
+    61,13,29
+    97,13,75,29,47
+"};
+
+#[test]
+fn part1() {
+    assert_eq!(solution::part1(EXAMPLE), 143);
+}
+
+#[test]
+fn part2() {
+    assert_eq!(solution::part2(EXAMPLE), 123);
+}
