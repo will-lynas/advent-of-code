cc9ecf7a0c179eba1b4ea4df35dcdfd16e320de4
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-07 17:41:07 +0000

    Allow 2 lints about casting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![warn(clippy::pedantic)]
 #![allow(clippy::must_use_candidate)]
 #![allow(clippy::missing_panics_doc)]
+// TODO: enable these two again
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::cast_possible_wrap)]
 
 pub mod utils;
 
