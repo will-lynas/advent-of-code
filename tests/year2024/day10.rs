3f85c6aab54663e04c423e6ce80be7b483f30cbb
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-10 22:51:09 +0000

    Add day10 tests

--- tests/year2024/day10.rs
+++ tests/year2024/day10.rs
@@ -1,11 +1,21 @@
 use advent_of_code::year2024::day10 as solution;
+use indoc::indoc;
 
-const EXAMPLE: &str = "Hello, World!";
+const EXAMPLE: &str = indoc! {"
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+"};
 
 #[test]
 fn part1() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    assert_eq!(solution::part1(&input), 36);
 }
 
 #[test]
