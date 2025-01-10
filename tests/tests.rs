607cd1a809d1bb402162f708fe803128b7dfac13
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-10 22:46:06 +0000

    Delete test template comment

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,23 +1,3 @@
-// Template:
-
-// use indoc::indoc;
-//
-// use advent_of_code::year2024::day01 as solution;
-//
-// const EXAMPLE: &str = indoc! {"
-//     TESTING
-// "};
-//
-// #[test]
-// fn part1() {
-//     assert_eq!(solution::part1(EXAMPLE), 69);
-// }
-//
-// #[test]
-// fn part2() {
-//     assert_eq!(solution::part2(EXAMPLE), 420);
-// }
-
 macro_rules! test {
     ($year:tt $($day:tt),*) => {
         pub mod $year {$(pub mod $day;)*}
