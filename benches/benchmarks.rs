2208c7bf3c2620a97e2d0c951a314b8ccdc167f3
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-10 22:16:25 +0000

    Add day 10 as a template

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -39,5 +39,5 @@ macro_rules! benchmark {
 }
 
 benchmark!(year2024
-    day01, day02, day03, day04, day05, day06, day07, day08, day09
+    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10
 );
