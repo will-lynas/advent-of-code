6c4ab7bc3a0bc5a25718afbafb057eb4eea68bb6
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-11 12:14:17 +0000

    Solve day10 part1

--- src/year2024/day10.rs
+++ src/year2024/day10.rs
@@ -1,13 +1,51 @@
-type Input = String;
+use std::collections::VecDeque;
+
+use gxhash::{
+    HashSet,
+    HashSetExt,
+};
+
+use crate::utils::{
+    grid::Grid,
+    point::Point,
+};
+
+type Input = Grid<u8>;
 
 pub fn parse(input: &str) -> Input {
-    input.into()
+    Grid::parse(input)
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+pub fn part1(grid: &Input) -> usize {
+    let mut count = 0;
+    for point in grid.points() {
+        if grid[point] == b'0' {
+            count += score(grid, point);
+        }
+    }
+    count
 }
 
-pub fn part2(input: &Input) -> usize {
-    input.len()
+pub fn score(grid: &Grid<u8>, point: Point) -> usize {
+    let mut seen = HashSet::new();
+    let mut q = VecDeque::new();
+    q.push_back(point);
+    while let Some(current) = q.pop_front() {
+        let valid_next: Vec<_> = grid
+            .adjacent(current)
+            .into_iter()
+            .filter(|next| grid[*next] == grid[current] + 1 && !seen.contains(next))
+            .collect();
+        for next in valid_next {
+            q.push_back(next);
+            seen.insert(next);
+        }
+    }
+    seen.into_iter()
+        .filter(|point| grid[*point] == b'9')
+        .count()
+}
+
+pub fn part2(_input: &Input) -> usize {
+    0
 }
