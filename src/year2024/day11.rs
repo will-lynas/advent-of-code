385ddb2d6f9fa2f27724014c7233c58dcc50fbd0
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-12 11:28:32 +0000

    Solve day 11 part 1

--- src/year2024/day11.rs
+++ src/year2024/day11.rs
@@ -7,9 +7,32 @@ pub fn parse(input: &str) -> Input {
         .collect()
 }
 
+fn split_digits(n: u64) -> Option<(u64, u64)> {
+    let n = n.to_string();
+    let l = n.len();
+    (l % 2 == 0).then(|| {
+        let (a, b) = n.split_at(l / 2);
+        (a.parse().unwrap(), b.parse().unwrap())
+    })
+}
+
 pub fn part1(input: &Input) -> usize {
-    println!("{input:?}");
-    input.len()
+    let mut stones = input.clone();
+    for _ in 0..25 {
+        let mut new_stones = Vec::new();
+        for &stone in &stones {
+            if stone == 0 {
+                new_stones.push(1);
+            } else if let Some(res) = split_digits(stone) {
+                new_stones.push(res.0);
+                new_stones.push(res.1);
+            } else {
+                new_stones.push(stone * 2024);
+            }
+        }
+        stones = new_stones;
+    }
+    stones.len()
 }
 
 pub fn part2(input: &Input) -> usize {
