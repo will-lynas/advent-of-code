8e79a421bcf36de1c89c612cb94d28d8df7ad5b1
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-13 22:20:45 +0000

    Change edges from a HashSet to just a Vec

--- src/year2024/day12.rs
+++ src/year2024/day12.rs
@@ -9,7 +9,7 @@ use crate::utils::{
     point::Point,
 };
 
-type Input = Vec<(u8, usize, HashSet<Edge>)>;
+type Input = Vec<(u8, usize, Vec<Edge>)>;
 
 pub fn parse(input: &str) -> Input {
     let grid = Grid::parse(input);
@@ -19,7 +19,7 @@ pub fn parse(input: &str) -> Input {
         if visited.contains(&point) {
             continue;
         }
-        let mut edges = HashSet::new();
+        let mut edges = Vec::new();
         let mut area = 0;
         let plant = grid[point];
         dfs(&grid, point, plant, &mut visited, &mut edges, &mut area);
@@ -33,7 +33,7 @@ fn dfs(
     point: Point,
     plant: u8,
     visited: &mut HashSet<Point>,
-    edges: &mut HashSet<Edge>,
+    edges: &mut Vec<Edge>,
     area: &mut usize,
 ) {
     if visited.contains(&point) {
@@ -45,7 +45,7 @@ fn dfs(
         if grid.contains(adjacent) && grid[adjacent] == plant {
             dfs(grid, adjacent, plant, visited, edges, area);
         } else {
-            edges.insert(Edge::new(point, adjacent).unwrap());
+            edges.push(Edge::new(point, adjacent).unwrap());
         }
     }
 }
