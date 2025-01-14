34741a375da3345ed9c5f3158c18d36a7bc6d137
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-14 11:27:38 +0000

    Allow Edge's to be directed and remove methods

--- src/utils/edge.rs
+++ src/utils/edge.rs
@@ -11,34 +11,12 @@ use super::point::Point;
 
 #[derive(PartialOrd, Ord, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct Edge {
-    p1: Point,
-    p2: Point,
-}
-
-impl Edge {
-    pub fn new(p1: Point, p2: Point) -> Option<Self> {
-        ((p1 - p2).norm1() == 1).then(|| {
-            let (p1, p2) = if p1 < p2 { (p1, p2) } else { (p2, p1) };
-            Self { p1, p2 }
-        })
-    }
-
-    pub fn dir(&self) -> Point {
-        self.p1 - self.p2
-    }
-
-    pub fn connected(&self, other: Self) -> bool {
-        if self.dir() == other.dir() {
-            let diff = self.p1 - other.p1;
-            self.dir().orthogonal(diff) && diff.norm1() == 1
-        } else {
-            self.p1 == other.p1 || self.p1 == other.p2 || self.p2 == other.p1 || self.p2 == other.p2
-        }
-    }
+    pub p1: Point,
+    pub p2: Point,
 }
 
 impl Debug for Edge {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}-{:?}", self.p1, self.p2)
+        write!(f, "{:?}->{:?}", self.p1, self.p2)
     }
 }
