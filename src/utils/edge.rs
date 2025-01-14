3b94747db0dd204d51072b70efe6143f817da03e
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-14 11:32:31 +0000

    Add back new fn for Edge

--- src/utils/edge.rs
+++ src/utils/edge.rs
@@ -15,6 +15,12 @@ pub struct Edge {
     pub p2: Point,
 }
 
+impl Edge {
+    pub fn new(p1: Point, p2: Point) -> Self {
+        Self { p1, p2 }
+    }
+}
+
 impl Debug for Edge {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{:?}->{:?}", self.p1, self.p2)
