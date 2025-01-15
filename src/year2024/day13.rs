7202f68eb8d617397994a4808101f7944f66806c
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-15 23:18:27 +0000

    Split out part 1

--- src/year2024/day13.rs
+++ src/year2024/day13.rs
@@ -24,36 +24,33 @@ pub fn parse(input: &str) -> Input {
         .collect()
 }
 
+fn solve((a, b, goal): (Point, Point, Point)) -> i32 {
+    // | a.x b.x | | ap |   | goal.x |
+    // | a.y b.y | | bp | = | goal.y |
+    //
+    // | ap |           | b.y  -b.x | | goal.x |
+    // | bp | = (1/det) | -a.y a.x  | | goal.y |
+
+    let det = a.x * b.y - a.y * b.x;
+    if det == 0 {
+        return 0;
+    }
+
+    let mut ap = goal.x * b.y - goal.y * b.x;
+    let mut bp = goal.y * a.x - goal.x * a.y;
+
+    if ap % det != 0 || bp % det != 0 {
+        return 0;
+    }
+
+    ap /= det;
+    bp /= det;
+
+    ap * 3 + bp
+}
+
 pub fn part1(input: &Input) -> i32 {
-    input
-        .iter()
-        .map(|claw| {
-            let (a, b, goal) = claw;
-
-            // | a.x b.x | | ap |   | goal.x |
-            // | a.y b.y | | bp | = | goal.y |
-            //
-            // | ap |           | b.y  -b.x | | goal.x |
-            // | bp | = (1/det) | -a.y a.x  | | goal.y |
-
-            let det = a.x * b.y - a.y * b.x;
-            if det == 0 {
-                return 0;
-            }
-
-            let mut ap = goal.x * b.y - goal.y * b.x;
-            let mut bp = goal.y * a.x - goal.x * a.y;
-
-            if ap % det != 0 || bp % det != 0 {
-                return 0;
-            }
-
-            ap /= det;
-            bp /= det;
-
-            ap * 3 + bp
-        })
-        .sum()
+    input.iter().copied().map(solve).sum()
 }
 
 pub fn part2(input: &Input) -> usize {
