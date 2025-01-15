04632ae7afae9e57ea87193c595992261f67cd69
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-01-15 22:58:08 +0000

    Day 13 parsing

--- src/year2024/day13.rs
+++ src/year2024/day13.rs
@@ -1,10 +1,31 @@
-type Input = String;
+use crate::utils::{
+    parsing::StringNumberParsing,
+    point::Point,
+};
+
+type Input = Vec<(Point, Point, Point)>;
+
+fn parse_line(line: &str) -> Point {
+    let nums = line.unsigned_nums();
+    Point::new(nums[0] as i32, nums[1] as i32)
+}
 
 pub fn parse(input: &str) -> Input {
-    input.into()
+    input
+        .split("\n\n")
+        .map(|block| {
+            let mut lines = block.lines();
+            (
+                parse_line(lines.next().unwrap()),
+                parse_line(lines.next().unwrap()),
+                parse_line(lines.next().unwrap()),
+            )
+        })
+        .collect()
 }
 
 pub fn part1(input: &Input) -> usize {
+    dbg!(input);
     input.len()
 }
 
--- tests/year2024/day13.rs
+++ tests/year2024/day13.rs
@@ -1,11 +1,28 @@
 use advent_of_code::year2024::day13 as solution;
+use indoc::indoc;
 
-const EXAMPLE: &str = "Hello, World!";
+const EXAMPLE: &str = indoc! {"
+    Button A: X+94, Y+34
+    Button B: X+22, Y+67
+    Prize: X=8400, Y=5400
+
+    Button A: X+26, Y+66
+    Button B: X+67, Y+21
+    Prize: X=12748, Y=12176
+
+    Button A: X+17, Y+86
+    Button B: X+84, Y+37
+    Prize: X=7870, Y=6450
+
+    Button A: X+69, Y+23
+    Button B: X+27, Y+71
+    Prize: X=18641, Y=10279
+"};
 
 #[test]
 fn part1() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    assert_eq!(solution::part1(&input), 480);
 }
 
 #[test]
