4fc2ef2f27c49165aaa8e1fc95995f90b99d340b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-03-28 22:19:45 +0000

    Load session cookie

--- download.py
+++ download.py
@@ -1,3 +1,12 @@
 # Run `uv sync` then `uv run download.py`
+# Put AOC session cookie in .env under SESSION_COOKIE
 
-print("Hello, world!")
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+session_cookie = os.getenv("SESSION_COOKIE")
+assert session_cookie, "SESSION_COOKIE is not set"
+
+print(session_cookie)
