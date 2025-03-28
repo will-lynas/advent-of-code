d19db267efc0b475fce389d30031436d00cc86c0
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-03-28 22:22:37 +0000

    Test input downloading

--- download.py
+++ download.py
@@ -3,10 +3,19 @@
 
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 
 session_cookie = os.getenv("SESSION_COOKIE")
 assert session_cookie, "SESSION_COOKIE is not set"
 
-print(session_cookie)
+def make_url(year, day):
+    return f"https://adventofcode.com/{year}/day/{day}/input"
+
+url = make_url(2024, 7)
+
+response = requests.get(url, cookies={"session": session_cookie})
+response.raise_for_status()
+
+print(response.text)
